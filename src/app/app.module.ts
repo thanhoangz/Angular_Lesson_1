68c4637f86dfde07f331e6a3f69aee2ecee23fea
thanhoangz <xuanhoang.ks6@gmail.com>
2020-09-29 07:31:00 +0700

    components and property bindding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { CalculatorComponent } from './calculator/calculator.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NewComponentComponent, CalculatorComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/new-component/new-component.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'phong-new-component',
+  templateUrl: './new-component.component.html',
+  styleUrls: ['./new-component.component.scss', './phong-css.css'],
+})
+export class NewComponentComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+}
